893aa15dfbc67c1df13f57bcbb253a3f0ac150c5
Qiu Tian <qiu_tian@students.edu.sg>
2024-12-18 22:46:34 +0800

    Big Update

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include <chrono>
 #include <thread>
 #include <filesystem>
-#include <set>
+#include <map>
 #include <vector>
 //Namespaces
 using namespace std;
@@ -35,6 +35,15 @@ struct NPC
     int x;
     int y;
     steady_clock::time_point spawntime;
+};
+struct Monster
+{
+    int x;
+    int y;
+    steady_clock::time_point spawntime;
+    int damage;
+    int speed;
+    int despawntime;
 };
 void showLoading(SDL_Renderer* renderer,SDL_Event event, char* loadtext,char* fontpath, auto textrenderer,int duration,auto textures)
 {
@@ -86,6 +95,26 @@ bool checkKey(char* key, SDL_Event event)
     }
     return false;
 }
+void moveMonster(Monster& monster,int playerx,int playery,mt19937 gen,discrete_distribution<>move)
+{
+    if (move(gen))
+    {
+        if (monster.x < playerx)
+        {
+            monster.x+=monster.speed;
+        }else if (monster.x > playerx)
+        {
+            monster.x-=monster.speed;
+        }
+        if (monster.y < playery)
+        {
+            monster.y+=monster.speed;
+        }else if (monster.y > playery)
+        {
+            monster.y-=monster.speed;
+        }
+    }
+}
 //Check for collision
 bool checkCollision(int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2){
 
@@ -121,6 +150,10 @@ bool playerNearNpc(int playerx, int playery, int npcx, int npcy, int dist)
     auto ydist = (playery - npcy)* (playery-npcy);
     auto xdist = (playerx - npcx) * (playerx - npcx);
     return xdist + ydist < dist*dist;
+}
+int calculateTimeElapsed(steady_clock::time_point start)
+{
+    return duration_cast<milliseconds>(steady_clock::now() - start).count();
 }
 
 /*
@@ -167,6 +200,11 @@ int main(int argc, char* argv[])
     unordered_map<int,NPC> npcpos;
     unordered_map<int, unordered_map<int, array<int, 4>>> spritePos;
     unordered_map<int, pair<int,int>> luckyblockpos;
+    unordered_map<int,array<int,4>> healthpos;
+    map<int, pair<int,int>>inventory; // We initialise the inventory in the form of:{Slot No.:item id,count}
+    for (int i=0;i<9;i++)inventory[i] = {-1,0};
+    unordered_map<int,char*>items;
+    items[0] = "a";items[1] ="b",items[2] ="c";items[3]="d";items[4] = "e";items[5]="f";items[6]="g";items[7]="h";
     int direction = 3; //Forward:0, Left:1,Right:2,Back:3
     int animationnum = 1;
     int prevdirection = NULL;
@@ -178,12 +216,21 @@ int main(int argc, char* argv[])
     int maxluckyblock = 10;
     int luckyblockcount = 0;
     bool trade = false;
+    bool tradeguiopen = false;
+    bool luckyblockopened = false;
+    int luckyblockcycle = 1;
+    int obtained;
+    bool displayObtained = false;
+    int healthstate = 1;
+    int damagecooldown = 400;
     random_device rd;
     mt19937 gen(rd());
     discrete_distribution<> npcdistribuition {99.999 ,0.1};
     uniform_int_distribution<> npccoordsgen(30,370);
     discrete_distribution<> luckyblockgen{99.99,0.1};
     uniform_int_distribution<> luckyblockcoordgen{20,380};
+    uniform_int_distribution<> number{0,7};
+    discrete_distribution<>move{98,2};
     int npccount = 0;
     char* mode = "home";
     //Init sprite animations and positions
@@ -207,7 +254,7 @@ int main(int argc, char* argv[])
     spritePos[3][2] ={16,72,16,24};
     spritePos[3][3] ={32,72,16,24};
     spritePos[3][4] ={48,72,16,24};
-
+    for (int i = 0;i<7;i++)healthpos[i+1] = {0,8*i,20,8};
     //SDL INIT
     SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
     IMG_Init(IMG_INIT_PNG|IMG_INIT_JPG|IMG_INIT_WEBP);
@@ -226,11 +273,29 @@ int main(int argc, char* argv[])
     SDL_Texture* player = render_image.createTexture(renderer,"../player_updated.png");
     SDL_Texture* luckyblock = render_image.createTexture(renderer,"../image.png");
     SDL_Texture* tradetexture = rendertext.CreateText(renderer, font_path,"Press e to trade",SDL_Color{255,255,255,255},500);
+    SDL_Texture* menu = render_image.createTexture(renderer,"../gui.png");
+    SDL_Texture* luckyblockmsg = rendertext.CreateText(renderer,font_path,"You got a: ",SDL_Color{255,255,255,255},500);
+    SDL_Texture* luckyblockresult;
+    SDL_Texture* menubackground = render_image.createTexture(renderer,"../black.png");
+    SDL_Texture* monster = render_image.createTexture(renderer,"../image.png");
+    SDL_Texture* health = render_image.createTexture(renderer,"../health.png");
+    SDL_SetTextureAlphaMod(menubackground,175);
     uint32_t frameStart = SDL_GetTicks();
     uint32_t frameCount = 0;
+    Monster monster1;
+    monster1.damage = 100;
+    monster1.x = 300;
+    monster1.y = 300;
+    monster1.spawntime = steady_clock::now();
+    monster1.damage = 10;
+    monster1.speed = 1;
+    monster1.despawntime = 10000000;
     //Home Screen
     steady_clock::time_point EnterclickTime;
     steady_clock::time_point ExitclickTime;
+    steady_clock::time_point luckyblockpickup;
+    steady_clock::time_point displaytime;
+    steady_clock::time_point lastDamage = steady_clock::now();
     while (mode == "home")
     {
         bool Enterclick = false;
@@ -328,32 +393,51 @@ int main(int argc, char* argv[])
                 //Handle Movement Keys
                 if (event.type == SDL_KEYDOWN)
                 {
-                    if (checkKey("Left", event) || checkKey("A", event))
+                    if ((checkKey("Left", event) || checkKey("A", event)) && !tradeguiopen && !luckyblockopened)
                     {
                         direction = 1;
                         playerx -=playerx >15?7:0;
 
-                    }else if (checkKey("Right", event)|| checkKey("D", event))
+                    }else if ((checkKey("Right", event)|| checkKey("D", event))&& !tradeguiopen && !luckyblockopened)
                     {
                         direction = 2;
                         playerx +=playerx<330?7:0;
-                    }else if (checkKey("Up", event) || checkKey("W", event))
+                    }else if ((checkKey("Up", event) || checkKey("W", event))&& !tradeguiopen&& !luckyblockopened)
                     {
                         direction = 3;
                         playery -=7;
-                    }else if (checkKey("Down", event) || checkKey("S", event))
+                    }else if ((checkKey("Down", event) || checkKey("S", event))&& !tradeguiopen&& !luckyblockopened)
                     {
                         direction = 0;
                         playery +=7;
+                    }else if (trade&&checkKey("E", event)&& !luckyblockopened)
+                    {
+                        trade = false;
+                        tradeguiopen = true;
+                        cout << "Open" << endl;
+                    }else if (tradeguiopen && checkKey("Escape", event))
+                    {
+                        tradeguiopen = false;
+                        trade=true;
                     }
-
                     moving =true;
+
                 }
                 // Handle Quiting
                 if (event.type == SDL_QUIT)
                 {
                     SDL_Quit();
                     return 0;
+                }
+            }
+            moveMonster(monster1,playerx,playery,gen,move);
+            if (playerNearNpc(playerx,playery,monster1.x,monster1.y,30))
+            {
+                if (duration_cast<milliseconds>(steady_clock::now() - lastDamage).count() >= damagecooldown)
+                {
+                    healthstate%=7;
+                    healthstate++;
+                    lastDamage = steady_clock::now();
                 }
             }
             //Handle Movement animations
@@ -378,36 +462,42 @@ int main(int argc, char* argv[])
             //Check if the player is near the npc
             for (auto i:npcpos)
             {
-                if (playerNearNpc(playerx, playery, i.second.x,i.second.y,50))
+                if (playerNearNpc(playerx, playery, i.second.x,i.second.y,50) && !tradeguiopen)
                 {
                     trade =true;
-                    cout <<"det" << endl;
+                }else
+                {
+                    trade=false;
                 }
             }
             for (auto i = luckyblockpos.begin();i!=luckyblockpos.end();)
             {
-                if (playerNearNpc(playerx,playery,i->second.first,i->second.first,50))
+                if (checkCollision(playerx,playery,50,50,i->second.first,i->second.second,20,20))
                 {
-                    cout << "near" << endl;
+                    luckyblockresult = rendertext.CreateText(renderer,font_path,items[number(gen)],SDL_Color{255,255,255,255},500);
                      luckyblockcount--;
                     i =luckyblockpos.erase(i);
+                    luckyblockpickup = steady_clock::now();
+                    luckyblockopened = true;
                 }else{
                     ++i;
                 }
             }
             //Show All Textures
             SDL_SetRenderDrawColor(renderer, 196, 220, 195, 255);
-            //Render Things below this code
+            //Render Things below this codee
             SDL_RenderClear(renderer);
             render_image.showImage(renderer,background,0,0,600,600,NULL,NULL,NULL,NULL,SDL_FLIP_NONE);
-            if (trade)rendertext.DrawText(renderer,tradetexture,175,400,250,60);trade=false;
+            if (trade)rendertext.DrawText(renderer,tradetexture,175,400,250,60);
             render_image.showImage(renderer,player,playerx,playery,50,50,shown[0],shown[1],shown[2],shown[3],spriteflip);
+            render_image.showImage(renderer,health,playerx,playery-15,50,13,healthpos[healthstate][0],healthpos[healthstate][1],healthpos[healthstate][2],healthpos[healthstate][3],SDL_FLIP_NONE);
             for (auto i = npcpos.begin(); i != npcpos.end();)
             {
                 auto lifespanelapsed = duration_cast<seconds>(steady_clock::now() - i->second.spawntime).count();
                 //Despawn npc after 30s
                 if (lifespanelapsed > 10000)
                 {
+                    luckyblockopened = true;
                     npccount--;
                     cout << "Despawned" << endl;
                     i = npcpos.erase(i);
@@ -416,7 +506,62 @@ int main(int argc, char* argv[])
                     ++i;
                 }
             }
+            render_image.showImage(renderer,monster,monster1.x,monster1.y,50,50,NULL,NULL,NULL,NULL,SDL_FLIP_NONE);
             for (auto i :luckyblockpos)render_image.showImage(renderer,luckyblock,i.second.first,i.second.second,20,20,NULL,NULL,NULL,NULL,SDL_FLIP_NONE);
+            if (displayObtained && duration_cast<milliseconds>(steady_clock::now()-displaytime).count() <700)
+            {
+                rendertext.DrawText(renderer,luckyblockmsg,20,20,200,50);
+                luckyblockresult = rendertext.CreateText(renderer,font_path,items[obtained],SDL_Color{255,255,255,255},500);
+                rendertext.DrawText(renderer,luckyblockresult,250,20,50,50);
+            }
+            if (luckyblockopened)
+            {
+                auto elapsed = duration_cast<milliseconds>(steady_clock::now() - luckyblockpickup).count();
+                if (luckyblockcycle <  100 )
+                {
+                    rendertext.DrawText(renderer,luckyblockmsg,20,20,200,50);
+                    if (elapsed > 90)
+                    {
+                        obtained = number(gen);
+                        luckyblockresult = rendertext.CreateText(renderer,font_path,items[obtained],SDL_Color{255,255,255,255},500);
+                        luckyblockpickup = steady_clock::now();
+                        luckyblockcycle++;
+                    }else
+                    {
+                        rendertext.DrawText(renderer,luckyblockresult,250,20,50,50);
+
+                    }
+                }else
+                {
+                    for (auto i:inventory)
+                    {
+                        if (i.second.first == -1)
+                        {
+                            i.second.first = obtained;
+                            i.second.second = 1;
+                            cout << i.first << ": " << i.second.first <<", "<< i.second.second << endl;
+                            break;
+                        }
+                        if (i.second.first == obtained)
+                        {
+                            i.second.second++;
+                            cout << i.first << ": " << i.second.first <<", "<< i.second.second << endl;
+                            break;
+                        }
+                    }
+                    luckyblockcycle =1;
+                    luckyblockopened = false;
+                    displayObtained = true;
+                    displaytime = steady_clock::now();
+                }
+            }
+            //WARNING:The texture below should be the last thing that is rendered
+            if (tradeguiopen)
+            {
+                render_image.showImage(renderer,menubackground,0,0,600,600,NULL,NULL,NULL,NULL,SDL_FLIP_NONE);
+                render_image.showImage(renderer,menu,135,110,325,325,NULL,NULL,NULL,NULL,SDL_FLIP_NONE);
+                //TODO: Render the internal parts of the menu(trade spaces, trade button, add. info)
+            }
             SDL_RenderPresent(renderer);
             //Limit Fps
             uint32_t frameTimeEnd = SDL_GetTicks();
